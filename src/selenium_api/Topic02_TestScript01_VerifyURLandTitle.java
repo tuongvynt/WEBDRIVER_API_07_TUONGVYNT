2583e93bee769238efc887af7a0c233c34b8fff8
deeelder <nttvy301294@gmail.com>
2018-11-02 12:55:58 +0700

    Add new TC - Topic01_TestScript01_VerifyURLandTitle - Vy Nguyen

---
+++ src/selenium_api/Topic02_TestScript01_VerifyURLandTitle.java
@@ -0,0 +1,54 @@
+package selenium_api;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class Topic02_TestScript01_VerifyURLandTitle {
+	WebDriver driver;
+
+	@Test
+	public void TestScript01_VerifyURLandTitle() {
+		String homePageTitle = driver.getTitle();
+		Assert.assertEquals(homePageTitle, "Home page");
+		
+		// Click on Account button
+		driver.findElement(By.xpath("//span[@class=\"label\" and contains(text(), 'Account')]")).click();
+		driver.findElement(By.linkText("My Account")).click();
+		driver.findElement(By.xpath("//a[@title='Create an Account']")).click();;
+		
+		//Click back verify loginUrl
+		driver.navigate().back();
+		String loginPageUrl = driver.getCurrentUrl();
+		Assert.assertEquals(loginPageUrl,"http://live.guru99.com/index.php/customer/account/login/");
+		
+		//Click forward and verify Create account Url
+		driver.navigate().forward();
+		String createAccountPageUrl = driver.getCurrentUrl();
+		Assert.assertEquals(createAccountPageUrl,"http://live.guru99.com/index.php/customer/account/create/");
+	}
+
+	@BeforeClass
+	public void beforeClass() {
+
+		//System.setProperty("webdriver.chrome.driver", ".\\lib\\chromedriver.exe");
+		//driver = new ChromeDriver();
+
+		driver = new FirefoxDriver();
+		driver.get("http://live.guru99.com");
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+	}
+
+	@AfterClass
+	public void afterClass() {
+		driver.quit();
+	}
+
+}
